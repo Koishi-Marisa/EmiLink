d8b13ab008db9f310a7a189fa71f07afde83e2bc
XiaoWan <87607314+Koishi-Marisa@users.noreply.github.com>
2026-07-22 11:02:33 +0800

    Fix: Add stub classes for Mixin annotation processor validation

---
+++ src/stub/java/appeng/integration/modules/itemlists/EncodingHelper.java
@@ -0,0 +1,5 @@
+package appeng.integration.modules.itemlists;
+
+public class EncodingHelper {
+    public static void encodeProcessingRecipe(Object menu, Object inputs, Object outputs) {}
+}
---
+++ src/stub/java/com/wintercogs/beyonddimensions/common/menu/DimensionsCraftMenu.java
@@ -0,0 +1,8 @@
+package com.wintercogs.beyonddimensions.common.menu;
+
+import java.util.List;
+
+public class DimensionsCraftMenu {
+    public void cleanCraftSlots(boolean toStorageFirst) {}
+    public void transferRecipe(List<?> inputKeys, List<?> amount) {}
+}
